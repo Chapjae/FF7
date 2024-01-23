cd7251e7c40150a2341b1f23d856d7db0f2b19f0
Jay Liu <jayliu@Jays-MacBook-Air.local>
2024-01-23 12:58:20 -0800

    server.js file incomplete, will pick up later

--- FF7/server/server.js
+++ FF7/server/server.js
@@ -1,3 +1,23 @@
 const express = require("express");
-const { ApolloServer } = requestAnimationFrameuire("@apollo/server");
-const { expressMiddleWare } = require("@apollo/server/express4")
+const { ApolloServer } = require("@apollo/server");
+const {expressMiddleware} = require("@apollo/server/express4")
+const path = require("path");
+
+const {typeDefs, resolvers} = require('./schemas');
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const startApolloServer = async () => {
+    await server.start();
+
+    app.use(express.urlencoded({extended: false}));
+    app.use(express.json());
+
+    if (process.env.NODE_ENV === "production")
+}
